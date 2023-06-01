362c78325cf01a2fc24c5412d3ca8a40693003fa
1 Lorki <gopnik228nk@gmail.com>
2023-06-01 16:48:35 +0300

    Refactored layouts and add controllers for pages

--- frontend/controllers/MainMenu.cpp
+++ frontend/controllers/MainMenu.cpp
@@ -1,30 +1,15 @@
 #include <controllers/MainMenu.h>
 
-class MainMenuControllerEventHandler : public Rml::EventListener
-{
-    public:
-        virtual void ProcessEvent(Rml::Event& event)
-        {
-            MAINLOG->WriteStr("FOCUSED","\n");
-            
-        }
-};
-
 
 void MainMenuController::Initialize() 
 {
-    
-
     View* view = new View("mainmenu");
 	mainWindow->SetCurrentView(view);
 
     EventListenerInstancer event_listener_instancer;
 	Rml::Factory::RegisterEventListenerInstancer(&event_listener_instancer);
 
-	view->Load(fs::path(UIPATH / fs::path("MainMenu.rml")));
-    
-    
-    
+	view->Load(fs::path(UIPATH / fs::path("MainMenu.rml")));    
 
     view->Show();
 };
