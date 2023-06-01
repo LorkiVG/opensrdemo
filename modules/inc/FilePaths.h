214387b3a99301d4f0efdce3cb36a6196d88ff02
1 Lorki <gopnik228nk@gmail.com>
2023-06-01 20:54:44 +0300

    Added doxygen to FilePaths

--- modules/inc/FilePaths.h
+++ modules/inc/FilePaths.h
@@ -5,23 +5,23 @@ namespace fs = boost::filesystem;
 
 using namespace std;
 
-//Путь к папке с выходным файлом игры
+/// @brief Путь к папке с выходным файлом игры
 extern const fs::path MAINPATH;
-//Путь к папке с ресурсами игры
+/// @brief Путь к папке с ресурсами игры
 extern const fs::path RESPATH;
-//Путь к папке с графическими ресурсами игры
+/// @brief Путь к папке с графическими ресурсами игры
 extern const fs::path DATAPATH;
-//Путь к папке с ресурсами игры
+/// @brief Путь к папке с ресурсами игры
 extern const fs::path FONTSPATH;
-//Путь к папке с логами
+/// @brief Путь к папке с логами
 extern const fs::path LOGPATH;
-//Путь к папке с текстовыми ресурсами игры
+/// @brief Путь к папке с текстовыми ресурсами игры
 extern const fs::path CFGPATH;
-//Путь к папке с языками
+/// @brief Путь к папке с языками
 extern const fs::path LANGSPATH;
-//Путь к папке с текущим выбраным языком, подгружается на этапе загрузки настроек
+/// @brief Путь к папке с текущим выбраным языком, подгружается на этапе загрузки настроек
 extern fs::path LANGPATH;
-//Путь к папке со стилями относительно выбранного языка
+/// @brief Путь к папке со стилями относительно выбранного языка
 extern fs::path STYLESPATH;
-//Путь к папке с rml
+/// @brief Путь к папке с rml 
 extern fs::path UIPATH;
